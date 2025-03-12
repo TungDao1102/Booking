be82586692187ce3aa7b1ea28f914a8b77ae352f
Tung Dao <daotung492@gmail.com>
2025-03-12 23:23:08 +0700

    add architecture testing

---
+++ test/Booking.ArchitectureTests/Domain/DomainTest.cs
@@ -0,0 +1,23 @@
+﻿using Booking.ArchitectureTests.Infrastructure;
+using Booking.Domain.Abstractions;
+using FluentAssertions;
+using NetArchTest.Rules;
+
+namespace Booking.ArchitectureTests.Domain
+{
+    public class DomainTest : BaseTest
+    {
+        [Fact]
+        public void DomainEvents_Should_BeSealed()
+        {
+            TestResult result = Types.InAssembly(DomainAssembly)
+                .That()
+                .ImplementInterface(typeof(IDomainEvent))
+                .Should()
+                .BeSealed()
+                .GetResult();
+
+            result.IsSuccessful.Should().BeTrue();
+        }
+    }
+}
---
+++ test/Booking.ArchitectureTests/Infrastructure/BaseTest.cs
@@ -0,0 +1,20 @@
+﻿using Booking.API;
+using Booking.Application;
+using Booking.Domain;
+using Booking.Infrastructure;
+using System.Reflection;
+
+namespace Booking.ArchitectureTests.Infrastructure
+{
+
+    public abstract class BaseTest
+    {
+        protected static readonly Assembly ApplicationAssembly = typeof(IApplicationAssemblyMarker).Assembly;
+
+        protected static readonly Assembly DomainAssembly = typeof(IDomainAssemblyMarker).Assembly;
+
+        protected static readonly Assembly InfrastructureAssembly = typeof(IInfrastructureAssemblyMarker).Assembly;
+
+        protected static readonly Assembly PresentationAssembly = typeof(IApiAssemblyMarker).Assembly;
+    }
+}
